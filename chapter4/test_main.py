900409888607182c368ce867a314382fd909e8ed
Javier Cisneros <>
2025-04-01 21:01:54 -0500

    test main

--- chapter4/test_main.py
+++ chapter4/test_main.py
@@ -33,7 +33,33 @@ def test_read_performaces():
     assert len(response.json()) == 17306
 
 
+# test /v0/performances/ with changed date
+def test_read_performaces_by_date():
+    response = client.get("\
+    /v0/performances/?skip=0&limit=2000&min_last_changed_date=2024-04-01")
+    assert response.status_code == 200
+    assert len(response.json()) == 2711
 
-# test /v0/performances/ with changed 2025-03-31
+
+# test /v0/leagues/{league_id}
+def test_read_leagues_with_id():
+    response = client.get("/v0/leagues/5002")
+    assert response.status_code == 200
+    assert len(response.json()) == 5
 
 
+# test /v0/teams
+def test_read_teams_for_one_league():
+    response = client.get("/v0/teams/?skip=0&limit=500&league_id=5001")
+    assert response.status_code == 200
+    assert len(response.json()) == 12
+
+
+# test the count functions
+def test_counts():
+    response = client.get("/v0/counts/")
+    response_data = response.json()
+    assert response.status_code == 200
+    assert response_data["league_count"] == 5
+    assert response_data["team_count"] == 20
+    assert response_data["player_count"] == 1018
